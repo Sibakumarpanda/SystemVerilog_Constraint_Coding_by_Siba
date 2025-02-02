b9d4baf5a395a106df56ae148046996f71b7b672
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-02-02 23:16:06 +0530

    Add files via upload
    Updated the solution for Question1 at initial level

---
+++ 1_testbench.sv
@@ -0,0 +1,26 @@
+// Question1: WAC to generate Even numbers in range of 1-100
+class my_packet;
+  
+  rand bit [3:0] a; 
+  
+  constraint c1 {a >1; a< 100;}
+  constraint c2 {a %2 ==0;}
+  
+endclass
+
+
+module tb_top;
+  
+  initial begin
+    
+    my_packet pkt;
+    pkt=new();
+    
+    for (int i; i<15;i++) begin
+      pkt.randomize();
+      $display("a=%0d",pkt.a);
+    end
+    
+  end
+  
+endmodule
