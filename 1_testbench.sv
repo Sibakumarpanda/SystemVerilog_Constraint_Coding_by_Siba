40bb729412502485b1218c6a7a301ebfdb575e07
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-02-08 22:20:34 +0530

    Update 1_testbench.sv
    size declaration of variable "a" is done properly, so as to generate the even nos in between 1-100

--- 1_testbench.sv
+++ 1_testbench.sv
@@ -1,7 +1,7 @@
 // Question1: WAC to generate Even numbers in range of 1-100
 class my_packet;
   
-  rand bit [3:0] a; 
+  rand bit [6:0] a; 
   
   constraint c1 {a >1; a< 100;}
   constraint c2 {a %2 ==0;}
