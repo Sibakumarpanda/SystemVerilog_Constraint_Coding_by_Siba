c87d8b0e4f633de1f17d692b08744d545c92b951
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-02-21 07:27:21 +0530

    Update 6b_testbench.sv
    Updated with alternative constraints for qstn 6b and committed

--- 6b_testbench.sv
+++ 6b_testbench.sv
@@ -10,6 +10,13 @@ class my_packet;
                 (a>>9) & (a>>10) & (a>>11)  
                 ) != 0;
                } 
+  
+  /* constraint c2 {   // if we will use this constraint then it will produce the consecutiveness from LSB side 12 1's
+                (a & (a>>1) & (a>>2) & (a>>3) & (a>>4) &
+                (a>>5) & (a>>6) & (a>>7) & (a>>8) &
+                (a>>9) & (a>>10) & (a>>11)  
+                ) == 1;
+               } */
 endclass
 
 module tb_top;
