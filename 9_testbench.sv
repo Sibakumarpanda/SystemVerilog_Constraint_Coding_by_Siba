c2ee70bd4a8beb065ec49acea875d0eebdfcd6ee
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-02-21 08:07:26 +0530

    Created 9_testbench.sv and committed the solution for qstn9

---
+++ 9_testbench.sv
@@ -0,0 +1,29 @@
+//9. WAC to generate 32-bit number with exactly one high bit.
+class my_packet;  
+  rand bit [31:0] a; 
+  constraint c1 {$onehot(a) ==1;}  
+endclass
+
+module tb_top;  
+  initial begin    
+    my_packet pkt;
+    pkt=new();    
+    for (int i=0; i<10;i++) begin
+      pkt.randomize();     
+      $display("a=%b",pkt.a);      
+    end    
+  end  
+endmodule  
+
+//Output
+a=00000000000010000000000000000000
+a=00000000000000000000001000000000
+a=00000001000000000000000000000000
+a=00000000000000000001000000000000
+a=00000000000000000000000100000000
+a=00000000000000000100000000000000
+a=00000000000000000000000000000001
+a=00000000100000000000000000000000
+a=00000000000000000010000000000000
+a=00000000000000010000000000000000
+           V C S   S i m u l a t i o n   R e p o r t 
