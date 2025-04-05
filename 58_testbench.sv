1d1118712686972509f093e11a22b7fbccbd684c
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-04-05 11:48:07 +0530

    Update 58_testbench.sv -Updated

--- 58_testbench.sv
+++ 58_testbench.sv
@@ -1,7 +1,7 @@
 //58.WAC to generate pattern 0,0,0,1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7
 class my_packet;  
   rand int d[]; 
-  constraint c1 {d.size() == 24;} // The pattern has 15 elements  
+  constraint c1 {d.size() == 24;} 
   constraint c2 { 
     foreach (d[i])      
       if (i<3)
