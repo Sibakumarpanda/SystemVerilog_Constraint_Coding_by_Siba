f3ce8a6442df3a1ebf74a43798e3734dc3affa5d
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-04-06 21:01:22 +0530

    Create 5b_testbench.sv -Committed the solution fr Qstn5b

---
+++ 5b_testbench.sv
@@ -0,0 +1,37 @@
+//5b.⁠WAC to generate 32-bit variable with exactly 16 consecutive 1's, starting from LSB position with using $countones
+class my_packet;
+  rand bit [31:0] value;
+  int start_pos; // It should not be random , so starting position=0 and it will start from it
+
+  // Constraint to ensure exactly 16 consecutive 1's starting from a random position
+  constraint consecutive_ones_constraint {
+    //start_pos inside {[0:16]};                              // Valid start positions for 16 consecutive 1's
+    value == ((32'hFFFF << start_pos) & 32'hFFFFFFFF);     // Shift 16 1's to the start position
+    $countones(value) == 16;                               // Ensure there are exactly 16 ones
+  }
+
+endclass
+
+module tb_top;
+  initial begin
+    my_packet pkt = new();
+    // Perform randomization
+    for (int i = 0; i < 10; i++) begin
+      assert(pkt.randomize()) 
+        $display("Randomized value: %b, Start position from LSB : %0d", pkt.value, pkt.start_pos);
+    end
+  end
+endmodule
+//output
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Apr  6 11:30 2025
+Randomized value: 00000000000000001111111111111111, Start position from LSB : 0
+Randomized value: 00000000000000001111111111111111, Start position from LSB : 0
+Randomized value: 00000000000000001111111111111111, Start position from LSB : 0
+Randomized value: 00000000000000001111111111111111, Start position from LSB : 0
+Randomized value: 00000000000000001111111111111111, Start position from LSB : 0
+Randomized value: 00000000000000001111111111111111, Start position from LSB : 0
+Randomized value: 00000000000000001111111111111111, Start position from LSB : 0
+Randomized value: 00000000000000001111111111111111, Start position from LSB : 0
+Randomized value: 00000000000000001111111111111111, Start position from LSB : 0
+Randomized value: 00000000000000001111111111111111, Start position from LSB : 0
+           V C S   S i m u l a t i o n   R e p o r t 
